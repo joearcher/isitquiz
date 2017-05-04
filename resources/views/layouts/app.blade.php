9cb0740404f5cbef52b50b7c94ddef6b549a3c47
Joe Archer <joe@laserred.co>
2017-05-04 08:55:45 +0100

    make yes rainbow animated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,5 +40,30 @@
     
     <script src="/js/app.js"></script>
     <script src="/js/selector.js"></script>
+    <script>
+    var step = 4, // colorChage step, use negative value to change direction
+    ms   = 10,  // loop every
+    $uni = $('.yes'),
+    txt  = $uni.text(),
+    len  = txt.length,
+    lev  = 360/len,
+    newCont = "",
+    itv;
+
+    for(var i=0; i<len; i++)newCont += "<span style='color:hsla("+ i*lev +", 100%, 50%, 1)'>"+ txt.charAt(i) +"</span>";
+
+    $uni.html(newCont); // Replace with new content
+    var $ch = $uni.find('span'); // character
+
+    function anim(){
+        itv = setInterval(function(){
+            $ch.each(function(){
+            var h = +$(this).attr('style').split(',')[0].split('(')[1]-step % 360;
+            $(this).attr({style:"color:hsla("+ h +", 100%, 50%, 1)"});
+            });
+        }, ms); 
+    }
+    anim();
+    </script>
 </body>
 </html>
